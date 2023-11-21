05a3cc9da6a030984600c94e440526120cef9a29
trinhac <cuong.ta14584@sinhvien.hoasen.edu.vn>
2023-11-21 18:35:25 +0700

    Commit #3: Add keyboard layout, fix the words array to make the word more easy, add view result modal, add background transition when click start

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
+
 const words = [
   "apple", "banana", "orange", "grape", "strawberry", "pineapple", "watermelon", "blueberry", "kiwi", "pear",
-  "apricot", "cherry", "coconut", "fig", "lemon", "lime", "mango", "papaya", "peach", "plum",
-  "raspberry", "blackberry", "cantaloupe", "cranberry", "nectarine", "pomegranate", "melon", "tangerine", "avocado", "guava",
-  "kiwifruit", "persimmon", "boysenberry", "elderberry", "gooseberry", "lychee", "passionfruit", "rhubarb", "starfruit", "durian",
-  "jackfruit", "kumquat", "mulberry", "plantain", "quince", "soursop", "ugli fruit", "yuzu", "zucchini", "eggplant",
-  "broccoli", "carrot", "cucumber", "lettuce", "spinach", "potato", "tomato", "bell pepper", "onion", "garlic",
-  "ginger", "cabbage", "cauliflower", "asparagus", "celery", "peas", "beans", "corn", "artichoke", "pumpkin",
-  "squash", "radish", "turnip", "sweet potato", "beetroot", "leek", "shallot", "fennel", "parsnip", "rutabaga",
-  "watercress", "arugula", "endive", "kale", "chard", "collard greens", "bok choy", "mustard greens", "broccolini", "daikon",
-  "edamame", "okra", "chives", "cilantro", "parsley", "basil", "mint", "thyme", "rosemary", "oregano"
-
+  "we", "he", "she", "it", "they", "this", "that", "these", "those", "some", "any", "all", "many", "few",
+  "big", "small", "good", "bad", "happy", "sad", "beautiful", "ugly", "hot", "cold", "new", "old", "young",
+  "man", "woman", "child", "dog", "cat", "bird", "fish", "house", "car", "tree", "flower", "sun", "moon",
+  "run", "walk", "jump", "eat", "drink", "sleep", "talk", "listen", "read", "write", "play", "work", "study"
 ];
 
+let isStared = false;
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
@@ -36,27 +32,67 @@ function setRandomWord() {
 setRandomWord();
 
 const typingInput = document.querySelector('.typing-text');
-const wordToType = document.getElementById('wordToType');
+// const wordToType = document.getElementById('wordToType');
 const startButton = document.getElementById('startButton');
 const timeDisplay = document.getElementById('timeDisplay');
-const wpmDisplay = document.getElementById('wpmDisplay');
+const viewResultButton = document.getElementById('viewResultButton');
+const resultSection = document.getElementById('resultSection');
+const wpmResult = document.getElementById('wpmResult');
 const gameText = document.querySelector('.game-text');
+const wordToType = document.getElementById('wordToType');
+const mistakesCount = document.getElementById('mistakesCount');
+const accuracyPercentage = document.getElementById('accuracyPercentage');
+const closeBtn = document.querySelector('.close');
+viewResultButton.addEventListener('click', function () {
+  resultModal.style.display = 'block';
+  calculateResults();
+});
+
+closeBtn.addEventListener('click', function () {
+  resultModal.style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === resultModal) {
+    resultModal.style.display = 'none';
+  }
+});
+
+function calculateResults() {
+  const endTime = new Date().getTime();
+  const minutes = (endTime - startTime) / 60000; // Calculate minutes
+  const typedWords = wordCount;
+  const accuracy = calculateAccuracy(typedWords, words.length);
+  const wpm = Math.round(typedWords / minutes); // Calculate WPM
+  const incorrectWords = typedWords - words.length;
+
+  wpmResult.textContent = wpm;
+  mistakesCount.textContent = incorrectWords;
+  accuracyPercentage.textContent = `${accuracy}%`;
+}
+
+function calculateAccuracy(typedWords, totalWords) {
+  const accuracy = ((totalWords - (typedWords - wordCount)) / totalWords) * 100;
+  return accuracy.toFixed(2);
+}
+
 
 let countdown;
-let timeLeft = 30;
+let timeLeft = 60;
 let startTime;
 let wordCount = 0;
 
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
   startGame();
 });
 
 function startGame() {
-  timeLeft = 30;
+  isStared = true;
+  timeLeft = 60;
   clearInterval(countdown);
   wordCount = 0;
   gameText.textContent = 'Game Started';
-  countdown = setInterval(function() {
+  countdown = setInterval(function () {
     timeLeft--;
     displayTime();
 
@@ -68,10 +104,10 @@ function startGame() {
       return;
     }
   }, 1000);
-  typingInput.addEventListener('input', function(event) {
+  typingInput.addEventListener('input', function (event) {
     const typedText = event.target.value.trim();
     const originalWord = wordToType.textContent.trim();
-  
+
     if (typedText === originalWord) {
       wordCount++;
       wordToType.textContent = getRandomWord();
@@ -83,13 +119,18 @@ function startGame() {
   startTime = new Date().getTime(); // Record start time
 }
 
-typingInput.addEventListener('input', function(event) {
-  const typedText = event.target.value.trim();
-  const originalWord = wordToType.textContent.trim();
+typingInput.addEventListener('input', function (event) {
+  if (isStared == true) {
+    return;
+  }
+  else {
+    const typedText = event.target.value.trim();
+    const originalWord = wordToType.textContent.trim();
 
-  if (typedText === originalWord) {
-    wordToType.textContent = getRandomWord();
-    event.target.value = '';
+    if (typedText === originalWord) {
+      wordToType.textContent = getRandomWord();
+      event.target.value = '';
+    }
   }
 });
 
@@ -97,14 +138,74 @@ function displayTime() {
   timeDisplay.textContent = `${timeLeft}s`;
 }
 
-function calculateWPM() {
-  const endTime = new Date().getTime();
-  const minutes = (endTime - startTime) / 60000; // Calculate minutes
-  const wpm = Math.round(wordCount / minutes); // Calculate WPM
 
-  wpmDisplay.textContent = `WPM: ${wpm}`;
+document.body.addEventListener('keydown', function () {
+  // Set the focus on the typing input field
+  typingInput.focus();
+});
+
+
+
+
+function getKey (e) {
+  var location = e.location;
+  var selector;
+  if (location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+      selector = ['[data-key="' + e.keyCode + '-R"]']
+  } else {
+      var code = e.keyCode || e.which;
+      selector = [
+          '[data-key="' + code + '"]',
+          '[data-char*="' + encodeURIComponent(String.fromCharCode(code)) + '"]'
+      ].join(',');
+  }
+  return document.querySelector(selector);
 }
 
+function pressKey (char) {
+  var key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
+  if (!key) {
+      return console.warn('No key for', char);
+  }
+  key.setAttribute('data-pressed', 'on');
+  setTimeout(function () {
+      key.removeAttribute('data-pressed');
+  }, 200);
+}
 
+document.body.addEventListener('keydown', function (e) {
+  var key = getKey(e);
+  if (!key) {
+      return console.warn('No key for', e.keyCode);
+  }
 
+  key.setAttribute('data-pressed', 'on');
+});
 
+document.body.addEventListener('keyup', function (e) {
+  var key = getKey(e);
+  key && key.removeAttribute('data-pressed');
+});
+
+function size () {
+  var size = keyboard.parentNode.clientWidth / 90;
+  keyboard.style.fontSize = size + 'px';
+  console.log(size);
+}
+
+var keyboard = document.querySelector('.keyboard');
+window.addEventListener('resize', function (e) {
+  size();
+});
+size();
+
+const body = document.body;
+
+startButton.addEventListener('click', function () {
+  body.classList.add('transition-background');
+  body.classList.add('dark-background');
+
+  setTimeout(() => {
+    body.classList.remove('dark-background');
+  }, timeLeft*1000); 
+});
